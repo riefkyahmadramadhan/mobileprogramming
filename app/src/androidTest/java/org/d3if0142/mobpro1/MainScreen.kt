961d3d38032d14675249b7645893253ee1778d07
Riefky Ahmad Ramadhan <riefky.ahmad11@gmail.com>
2024-03-14 13:22:23 +0700

    3.1.Memisahkan Composable dari Activity

---
+++ app/src/androidTest/java/org/d3if0142/mobpro1/MainScreen.kt
@@ -0,0 +1,2 @@
+package org.d3if0142.mobpro1
+
--- app/src/main/java/org/d3if0142/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if0142/mobpro1/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import org.d3if0142.mobpro1.ui.screen.MainScreen
 import org.d3if0142.mobpro1.ui.theme.Mobpro1Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,47 +30,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(content: @Composable (Modifier) -> Unit) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.app_name)) },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-    ) { paddingValues ->
-        content(Modifier.padding(paddingValues))
-
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    MainScreen { modifier ->
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun ScreenPreview() {
-    Mobpro1Theme {
-        Greeting("Android")
-    }
-}

d8ef3fe43e719c223ed35739a413c0ac7b41e290
riefkyahmadramadhan <riefky.ahmad11@gmail.com>
2024-03-28 13:42:21 +0700

    3.2.Membuat tampilan list

--- app/src/main/java/org/d3if0142/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0142/mobpro1/ui/screen/MainScreen.kt
@@ -1,9 +1,14 @@
 package org.d3if0142.mobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -13,9 +18,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.d3if0142.mobpro1.R
+import org.d3if0142.mobpro1.model.Catatan
 import org.d3if0142.mobpro1.ui.theme.Mobpro1Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -39,13 +48,40 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
-    Column (
-        modifier = modifier.fillMaxSize().padding(16.dp)
-    ){
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
 
+    LazyColumn (
+        modifier = modifier.fillMaxSize()
+    ){
+        items(data) {
+            ListItem(catatan = it)
+            Divider()
+        }
     }
-    
 }
+
+@Composable
+fun ListItem(catatan: Catatan) {
+    Column (
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Text(
+            text = catatan.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = catatan.catatan,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = catatan.tanggal)
+    }
+}
+
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)

e1c29711af8de623029396111e3e3f6ad67cca6f
riefkyahmadramadhan <riefky.ahmad11@gmail.com>
2024-03-28 14:16:49 +0700

    3.5.Membuat tombol FAB

--- app/src/main/java/org/d3if0142/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0142/mobpro1/ui/screen/MainScreen.kt
@@ -1,17 +1,23 @@
 package org.d3if0142.mobpro1.ui.screen
 
 import android.content.res.Configuration
+import android.graphics.drawable.Icon
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -34,6 +40,8 @@ import org.d3if0142.mobpro1.ui.theme.Mobpro1Theme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -43,9 +51,22 @@ fun MainScreen() {
                     titleContentColor = MaterialTheme.colorScheme.primary
                 )
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    Toast.makeText(context, R.string.tambah_error, Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(R.string.tambah_catatan),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
         }
-    ) { paddingValues ->
-        ScreenContent(Modifier.padding(paddingValues))
+    ) { padding ->
+        ScreenContent(Modifier.padding(padding))
 
     }
 }
@@ -68,7 +89,8 @@ fun ScreenContent(modifier: Modifier) {
         }
     } else {
         LazyColumn(
-            modifier = modifier.fillMaxSize()
+            modifier = modifier.fillMaxSize(),
+            contentPadding = PaddingValues(bottom = 84.dp)
         ) {
             items(data) {
                 ListItem(catatan = it){
@@ -84,7 +106,8 @@ fun ScreenContent(modifier: Modifier) {
 @Composable
 fun ListItem(catatan: Catatan, onClick: () -> Unit) {
     Column (
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .clickable { onClick() }
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
